aa2f9da3bdb6b3bceebde1330f6dfaf9f391b50f
Jason <jasonlikescook@gmail.com>
2020-12-02 18:25:21 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,43 +1,38 @@
+//todo, expired? set by orgid?
+function updateFilter(dash){
+    let filters = JSON.parse(localStorage.getItem('filters'));
+    dash.filters.update(filters, { refresh: true, save: true });
+}
+
 
 prism.on('dashboardloaded', (event, args) => {
-    console.log(`--------------------${args.dashboard.title}--------------------------`);
     const dashboard = args.dashboard;
-    let clearFlag = false;
+    let hasDefaultFilters = false;
+    let hasFilterCleared = false;
     dashboard.on('filterschanged', function (dash, args) {
-        console.log('++++++++++start filter changed++++++++++++++++++++++');
-        if(clearFlag){
-            console.log(`clearFlag is ${clearFlag}`);
-            clearFlag = false;
-            return;
+        if(hasDefaultFilters){
+            hasDefaultFilters = false;
+            hasFilterCleared = true;
+            dashboard.filters.clear();
+            return false;
         }
-        if(args.items.elecTrigger || args.items && args.items.length > 0 && args.items[0].elecTrigger){
-            console.log('find filter is triggered from localStorage, skip set localStorage');
-            return;
+        if(hasFilterCleared){
+            hasFilterCleared = false;
+            updateFilter(dash);
+            return false;
         }
-        console.log('start to set the filters');
-        console.log(dash.filters.$$items);
+        if(!dash.filters || dash.filters.length === 0){
+            console.error('filters is empty, and will be set to localStorage');
+        }
         localStorage.setItem('filters', JSON.stringify(dash.filters.$$items));
     });
 
     dashboard.on('initialized', function (dash, args) {
-        console.log('--------------------start initialized----------------------------');
-        if(dash.filters.$$items && dash.filters.$$items.length > 0){
-            clearFlag = true;
-            dashboard.filters.clear();
-        }   
-        setTimeout(function(){
-            // dashboard.filters.clear();
-            let filters = JSON.parse(localStorage.getItem('filters'));
-            console.log('get the filters from localStorage');
-            console.log(filters);
-            if(filters && filters.length > 0){
-                filters[0].elecTrigger = true;
-            }else{
-                filters.elecTrigger = true;
-            }
-            dashboard.filters.update(filters, { refresh: true, save: true });
-            // dashboard.filters.reset(filters);
-        }, 0);
-        
+        if(dash.filters && dash.filters.length > 0){
+            hasDefaultFilters = true;
+            return;
+        }else{
+            updateFilter(dash);
+        }
     });
 });
